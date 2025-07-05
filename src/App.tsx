fd4b4e808d05ca6f1366413611ff2bb59e0bee83
bloodnighttw <bbeenn1227@gmail.com>
2025-07-05 23:49:58 +0800

    feat: restructure App component links and add GeneratorWalker component

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,20 @@ import AsyncGeneratorWalker from "./utils/AsyncGenerator";
 function App() {
   return (
     <div className="App">
-      <a href="https://github.com/bloodnighttw/react-generator-demo"> see code </a>
-      <h1 className="text-4xl text-amber-300">React Generator Example</h1>
+      <a href="https://github.com/bloodnighttw/react-generator-demo">
+        see code{" "}
+      </a>
+      <a href="https://github.com/bloodnighttw/react-generator-demo/blob/main/src/Generator.tsx">
+        <h1 className="text-4xl text-amber-300">React Generator Example</h1>
+      </a>{" "}
       <Generator />
-      <h1 className="text-4xl text-amber-500">Async Generator Example</h1>
+      <a href="https://github.com/bloodnighttw/react-generator-demo/blob/main/src/AsyncGenerator.tsx">
+        <h1 className="text-4xl text-amber-500">Async Generator Example</h1>
+      </a>
       <AsyncGeneratorTest />
-      <h1>async walker generator</h1>
+      <a href="https://github.com/bloodnighttw/react-generator-demo/blob/main/src/GeneratorWalker.tsx">
+        <h1>async walker generator</h1>
+      </a>
       <AsyncGeneratorWalker generator={AsyncGenerator} />
     </div>
   );
---
+++ src/GeneratorWalker.tsx
@@ -0,0 +1,26 @@
+import type { JSX } from "react";
+import AsyncGeneratorWalker from "./utils/AsyncGenerator";
+import type { ReAsyncGenerator } from "./utils/useAsyncGenerator";
+import waitTime from "./utils/waitTime";
+
+
+export async function* AsyncGenerator(): ReAsyncGenerator<JSX.Element> {
+  const ms = 1500;
+
+  yield <div className="text-4xl text-amber-300">hi</div>;
+  await waitTime({ ms });
+  yield <div className="text-4xl text-amber-500">hello</div>;
+  await waitTime({ ms });
+  yield <div className="text-4xl text-amber-700">world</div>;
+  await waitTime({ ms });
+  yield <div className="text-4xl text-amber-900">!</div>;
+  await waitTime({ ms });
+  yield <div className="text-4xl text-amber-400">done</div>;
+  await waitTime({ ms });
+  return <div className="text-4xl text-amber-600">finished</div>;
+}
+
+
+export default function GeneratorWalker() {
+    return <AsyncGeneratorWalker generator={AsyncGenerator} />
+}
