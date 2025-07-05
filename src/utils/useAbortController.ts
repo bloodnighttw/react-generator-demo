3fb7a18f5799c9aa9372a8e2953f368c648d711e
bloodnighttw <bbeenn1227@gmail.com>
2025-07-05 21:22:04 +0800

    refactor: ensure consistent return of controller.signal in useAbortController

--- src/utils/useAbortController.ts
+++ src/utils/useAbortController.ts
@@ -3,4 +3,5 @@ import { useEffect, useState } from "react";
 export default function useAbortController() {
     const [controller] = useState(new AbortController());
     useEffect(()=> controller.abort(),[controller]);
+    return controller.signal;
 }
