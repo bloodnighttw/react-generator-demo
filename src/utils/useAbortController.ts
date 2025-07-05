57dc8a2ba133e5619dd6a1693b89b8df3ea0b7b5
bloodnighttw <bbeenn1227@gmail.com>
2025-07-05 21:22:40 +0800

    refactor: fix cleanup function in useAbortController to ensure proper aborting

--- src/utils/useAbortController.ts
+++ src/utils/useAbortController.ts
@@ -2,6 +2,6 @@ import { useEffect, useState } from "react";
 
 export default function useAbortController() {
     const [controller] = useState(new AbortController());
-    useEffect(()=> controller.abort(),[controller]);
+    useEffect(()=> ()=>controller.abort(),[controller]);
     return controller.signal;
 }
