5770637d5231427627971335ad9520b58b0c70ae
bloodnighttw <bbeenn1227@gmail.com>
2025-07-05 21:21:34 +0800

    feat: add useAbortController hook for managing abort signals in React

---
+++ src/utils/useAbortController.ts
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+
+export default function useAbortController() {
+    const [controller] = useState(new AbortController());
+    useEffect(()=> controller.abort(),[controller]);
+}
