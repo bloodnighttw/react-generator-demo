cd68be5f2022737561433c3a268a541a8a0c6c67
bloodnighttw <bbeenn1227@gmail.com>
2025-07-05 23:59:45 +0800

    fix: add target and rel attributes to external links in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,20 @@ import AsyncGeneratorWalker from "./utils/AsyncGenerator";
 function App() {
   return (
     <div className="App">
-      <a href="https://github.com/bloodnighttw/react-generator-demo">
-        see code{" "}
+      <a href="https://github.com/bloodnighttw/react-generator-demo" target="_blank" rel="noopener noreferrer">
+      see code{" "}
       </a>
-      <a href="https://github.com/bloodnighttw/react-generator-demo/blob/main/src/Generator.tsx">
-        <h1 className="text-4xl text-amber-300">React Generator Example</h1>
+      <h2>click demo title to open code example</h2>
+      <a href="https://github.com/bloodnighttw/react-generator-demo/blob/main/src/Generator.tsx" target="_blank">
+      <h1 className="text-4xl text-amber-300">React Generator Example</h1>
       </a>{" "}
       <Generator />
-      <a href="https://github.com/bloodnighttw/react-generator-demo/blob/main/src/AsyncGenerator.tsx">
-        <h1 className="text-4xl text-amber-500">Async Generator Example</h1>
+      <a href="https://github.com/bloodnighttw/react-generator-demo/blob/main/src/AsyncGenerator.tsx" target="_blank">
+      <h1 className="text-4xl text-amber-500">Async Generator Example</h1>
       </a>
       <AsyncGeneratorTest />
-      <a href="https://github.com/bloodnighttw/react-generator-demo/blob/main/src/GeneratorWalker.tsx">
-        <h1>async walker generator</h1>
+      <a href="https://github.com/bloodnighttw/react-generator-demo/blob/main/src/GeneratorWalker.tsx" target="_blank">
+      <h1>async walker generator</h1>
       </a>
       <AsyncGeneratorWalker generator={AsyncGenerator} />
     </div>
